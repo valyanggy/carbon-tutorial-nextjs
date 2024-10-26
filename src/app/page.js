c06a04e2571dce8edb0fe73c30d8a39a967ee558
valyanggy <66551446+valyanggy@users.noreply.github.com>
2024-10-26 16:26:41 -0400

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+'use client';
+import { Button } from '@carbon/react';
+import LandingPage from './home/page';
 
 export default function Home() {
   return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
+    //   <div>
+    //     <div>
+    // Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+    //     </div>
+    //     <Button>Button</Button>
 
-  )
+    //   </div>
+    <div>REPO PAGE</div>
+  );
 }
